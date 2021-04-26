0639e19e3d47bfcaab9e71c04d0e1241d9707a86
jurrem <jurre@moosty.com>
2021-04-26 11:41:33 +0200

    stories

--- src/stories/forms/SimpleInput.stories.js
+++ src/stories/forms/SimpleInput.stories.js
@@ -35,15 +35,9 @@ export const All = () => <div>
       tooltipText="Select on which cloud platform you want to deploy your app. You can always switch to a different platform at a later time."
     >
       <SimpleInput default label={"default"}/></FormElement>
-    <div className="w-1/2">
-      <SimpleInput default placeholder="10%" description descriptionMessage="description" selector number
-                   label={"datepicker mockup"}/>
-      <SimpleInput default placeholder="02/02/1988" description descriptionMessage="description" datePicker
-                   label={"datepicker mockup"}/>
-    </div>
+
     <SimpleInput default label={"default + placeholder"} placeholder="your name here"/>
     <SimpleInput default label={"default + placeholder + icon"} infoIcon placeholder="your name here"/>
-    <SimpleInput default label={"default"} shadow/>
 
     <Typography type="h2" Element='h1'>
       Filled field </Typography>
@@ -72,20 +66,44 @@ export const All = () => <div>
     <SimpleInput default label={"default + placeholder + icon"} infoIcon placeholder="your name here"/>
     <SimpleInput default label={"default + shadow"} shadow/>
 
-    <Typography type="h3" Element='h3'>
-      Filled field </Typography>
 
-    <SimpleInput filled label={"filled"}/>
-    <SimpleInput filled label={"filled + placeholder"} placeholder="your name here"/>
-
-    <SimpleInput readOnly label={"read only"} placeholder="your name here"/>
-    <SimpleInput disabled label={"disabled"} placeholder="your name here"/>
-
-    <Typography type="h3" Element='h3'>
-      Static images of dynamic states - Hover, focused, error </Typography>
-    <SimpleInput hover label={"hover"} infoIcon/>
-    <SimpleInput focused label={"focused"} infoIcon/>
-    <SimpleInput error label={"error"} infoIcon errorMessage="Check this field again"/>
   </div>
 
+</div>
+
+export const ShadowState = () => <div className="space-y-3 ">
+  <Typography type="h3" Element='h3'>Shadow field </Typography>
+  <SimpleInput default label={"default"} shadow/>
+  
+  <FormElement
+    label="Description (optional)" descriptionTop="description Top" descriptionBottom="descriptionBottom"
+    infoIcon
+    tooltipText="Select on which cloud platform you want to deploy your app. You can always switch to a different platform at a later time."
+  >
+    <SimpleInput default label={"default"} shadow/>
+  </FormElement>
+</div>
+
+export const FilledStates = () => <div className="space-y-3 ">
+  <Typography type="h3" Element='h3'>Filled field </Typography>
+  <SimpleInput filled label={"filled"}/>
+  <SimpleInput filled label={"filled + placeholder"} placeholder="your name here"/>
+  <SimpleInput readOnly label={"read only"} placeholder="your name here"/>
+  <SimpleInput disabled label={"disabled"} placeholder="your name here"/>
+</div>
+export const States = () => <div className="space-y-3 ">
+  <Typography type="h4" Element='h3'>
+    Static images of dynamic states - Hover, focused, error </Typography>
+  <SimpleInput hover label={"hover"} infoIcon/>
+  <SimpleInput focused label={"focused"} infoIcon/>
+  <SimpleInput error label={"error"} infoIcon errorMessage="Check this field again"/>
+</div>
+
+
+export const Specials = () => <div className="space-y-3 w-1/5 justify-center">
+  <Typography type="h3" Element='h3'>Specials field </Typography>
+  <SimpleInput default placeholder="10%" description descriptionMessage="description" selector number
+               label={"datepicker mockup"}/>
+  <SimpleInput default placeholder="02/02/1988" description descriptionMessage="description" datePicker
+               label={"datepicker mockup"}/>
 </div>
