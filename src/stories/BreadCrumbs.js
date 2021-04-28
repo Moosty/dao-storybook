aa57a8e7d0091c32290aa0ed1da44a9e260093f3
corbifex <sander@moosty.com>
2021-04-28 10:45:03 +0200

    breadcrumbs

--- src/stories/BreadCrumbs.js
+++ src/stories/BreadCrumbs.js
@@ -1,42 +1,55 @@
 import React from "react";
 import {ChevronRightIcon, HomeIcon} from '@heroicons/react/solid'
+import PropTypes from "prop-types";
+import {CardFooter} from "./cards/CardFooter";
 
-const pages = [
-  {name: "Daos", href: '#', current: false},
-  {name: 'LCU Dao', href: '#', current: true},
-]
-
-export const BreadCrumbs = ({className}) => {
-  return (
-    <nav className={[
-      className,
-      "flex",
-    ].join(" ")}
-         aria-label="Breadcrumb">
-      <ol className="flex items-center space-x-4">
-      <li>
-      <div>
-      <a href="#" className="text-gray-400 hover:text-gray-500">
-      <HomeIcon className="flex-shrink-0 h-5 w-5" aria-hidden="true" />
-      <span className="sr-only">Home</span>
-      </a>
-      </div>
-      </li>
-    {pages.map((page) => (
-      <li key={page.name}>
-      <div className="flex items-center">
-      <ChevronRightIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-      <a
-      href={page.href}
-      className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
-      aria-current={page.current ? 'page' : undefined}
-      >
-    {page.name}
-      </a>
-      </div>
-      </li>
+export const BreadCrumbs = ({className, crumbs}) => (
+  <nav className={[
+    className,
+    "flex",
+  ].join(" ")}
+       aria-label="Breadcrumb">
+    <ol className="flex items-center space-x-4">
+      {crumbs.map((crumb, i) => (
+        <li key={crumb.name}>
+          <div className="flex items-center">
+            {i > 0 && <ChevronRightIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true"/>}
+            <span
+              onClick={crumb.onClick}
+              className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700 cursor-pointer"
+              aria-current={crumb.current ? 'page' : undefined}
+            >
+              {i === 0 && <HomeIcon className="flex-shrink-0 h-5 w-5" aria-hidden="true"/>}
+              {i === 0 && <span className="sr-only">{crumb.name}</span>}
+              {i > 0 && crumb.name}
+            </span>
+          </div>
+        </li>
       ))}
-      </ol>
-      </nav>
-      )
-    }
+    </ol>
+  </nav>
+)
+
+BreadCrumbs.propTypes = {
+  className: PropTypes.string,
+  crumbs: PropTypes.array,
+};
+
+BreadCrumbs.defaultProps = {
+  className: "",
+  crumbs: [
+    {
+      name: "Home",
+      onClick: () => console.log("goto home"),
+    },
+    {
+      name: "Page",
+      onClick: () => console.log("goto Page"),
+    },
+    {
+      name: "Sub Page",
+      onClick: () => console.log("goto sub page"),
+      current: true,
+    },
+  ]
+};
