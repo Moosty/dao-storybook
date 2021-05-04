11323bb2751431202b62271affc8b88993da85ca
jurrem <jurre@moosty.com>
2021-05-04 17:23:49 +0200

    stories

--- src/stories/crowd/CrowdCardContainer.stories.js
+++ src/stories/crowd/CrowdCardContainer.stories.js
@@ -22,3 +22,83 @@ Playground.args = {
   projectImages: projectImages,
 }
 
+const Template = (args) => <CrowdCardContainer {...args}/>
+
+export const Preview = Template.bind({})
+Preview.args = {
+    title: "Project XXXXXXX",
+    category: 2,
+    owner: "lsk13212341dfs23567246sdg",
+    targetAmount: 10000,
+    projectUrl: "https://moosty.com/",
+    state: "preview",
+    image: 1,
+  }
+
+export const Open = Template.bind({})
+Open.args = {
+  title: "Project Trust in ICOs",
+  category: 2,
+  owner: "lsk13212341dfs23567246sdg",
+  targetAmount: 10000,
+  projectUrl: "https://moosty.com/",
+  state: "open",
+  image: 1,
+}
+
+export const Pending = Template.bind({})
+Pending.args = {
+  title: "Project XXXXXXX",
+  category: 2,
+  owner: "lsk13212341dfs23567246sdg",
+  targetAmount: 10000,
+  projectUrl: "https://moosty.com/",
+  state: "open",
+  image: 1,
+}
+
+export const Active = Template.bind({})
+Active.args = {
+  ...Preview.args,
+  state: "active.active",
+}
+
+export const ActivePending = Template.bind({})
+ActivePending.args = {
+  ...Preview.args,
+  state: "active.pending",
+}
+
+export const ActiveVoting = Template.bind({})
+ActiveVoting.args = {
+  ...Preview.args,
+  state: "active.voting",
+}
+
+export const ActiveClaiming = Template.bind({})
+ActiveClaiming.args = {
+  ...Preview.args,
+  state: "active.claiming",
+}
+
+
+export const Failed = Template.bind({})
+Failed.args = {
+  ...Preview.args,
+  state: "active.failed",
+}
+
+
+export const Ended = Template.bind({})
+Ended.args = {
+  ...Preview.args,
+  state: "active.ended",
+}
+
+
+export const Canceled = Template.bind({})
+Canceled.args = {
+  ...Preview.args,
+  state: "active.canceled",
+}
+
