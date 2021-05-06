59e143da530fdc91887739190a023b3cddf91725
corbifex <sander@moosty.com>
2021-05-06 14:58:54 +0200

    up version

--- src/stories/forms/InputAvatar.js
+++ src/stories/forms/InputAvatar.js
@@ -22,7 +22,6 @@ export const InputAvatar = ({
                             }) => {
   const [selected, setSelected] = useState(selectedItem);
   const {formClass} = useForms({disabled, error, readOnly});
-  console.log(items)
   return (
     <Listbox
       value={selected}
@@ -79,45 +78,42 @@ export const InputAvatar = ({
                 dropDownShadow && "shadow-defaultPrimary",
               ].join(" ")}
             >
-              {items && items.map((item) => {
-                console.log(item.name)
-                return (
-                  <Listbox.Option
-                    key={item?.id}
-                    className={({active}) =>
-                      classNames(
-                        active ? 'text-white bg-formActive' : 'text-textBody',
-                        'cursor-default select-none relative py-2 pl-3 pr-9'
-                      )
-                    }
-                    value={item}
-                  >
-                    {({selected, active}) => (
-                      <>
-                        <div className="flex items-center">
-                          {item?.icon && <img src={item?.icon} alt="" className="flex-shrink-0 h-6 w-6 rounded-full"/>}
-                          <span
-                            className={[
-                              selected ? 'font-semibold' : 'font-normal',
-                              'ml-3 block',
-                            ].join(" ")}
-                          >
-                          {item?.name}
-                        </span>
-                        </div>
-                        {selected && <span
+              {items && items.map((item) => (
+                <Listbox.Option
+                  key={item?.id}
+                  className={({active}) =>
+                    classNames(
+                      active ? 'text-white bg-formActive' : 'text-textBody',
+                      'cursor-default select-none relative py-2 pl-3 pr-9'
+                    )
+                  }
+                  value={item}
+                >
+                  {({selected, active}) => (
+                    <>
+                      <div className="flex items-center">
+                        {item?.icon && <img src={item?.icon} alt="" className="flex-shrink-0 h-6 w-6 rounded-full"/>}
+                        <span
                           className={[
-                            active ? 'text-white' : 'text-indigo-600',
-                            'absolute inset-y-0 right-0 flex items-center pr-4',
+                            selected ? 'font-semibold' : 'font-normal',
+                            'ml-3 block',
                           ].join(" ")}
                         >
+                          {item?.name}
+                        </span>
+                      </div>
+                      {selected && <span
+                        className={[
+                          active ? 'text-white' : 'text-indigo-600',
+                          'absolute inset-y-0 right-0 flex items-center pr-4',
+                        ].join(" ")}
+                      >
                         <CheckIcon className="h-5 w-5" aria-hidden="true"/>
                       </span>}
-                      </>
-                    )}
-                  </Listbox.Option>
-                )
-              })}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
             </Listbox.Options>
           </Transition>
         </div>
