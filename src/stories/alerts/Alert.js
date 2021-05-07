73ad6ab8163092d65778fd9efd8105d6fd7a6078
jurrem <jurre@moosty.com>
2021-05-07 16:22:41 +0200

    alerts aangepast

--- src/stories/alerts/Alert.js
+++ src/stories/alerts/Alert.js
@@ -7,32 +7,34 @@ import {
   XIcon
 } from '@heroicons/react/solid'
 
-export const Alert = ({warning, error, success, label, topRight, topLeft, bottomLeft}) => {
+export const Alert = ({warning, error, state, position, success, label, topRight, topLeft, bottomLeft}) => {
 
   return (<div className={["rounded-default p-4 z-50 shadow-defaultPrimary  w-1/3",
-      error ? "bg-dangerBackground" :
-        warning ? "bg-warningBackground" :
-          success ? "bg-successBackground" :
+      state === 'error' ? "bg-dangerBackground" :
+        state === 'warning' ? "bg-warningBackground" :
+          state === 'success' ? "bg-successBackground" :
             "bg-infoBackground",
-      topRight ? "absolute top-20 right-5" :
-        topLeft ? "absolute top-20 left-5" :
-          bottomLeft ? "absolute bottom-0 left-5" : " ",
+      position === 'topRight' ? "absolute top-20 right-5" :
+        position === 'topLeft' ? "absolute top-20 left-5" :
+         position === 'bottomLeft' ? "absolute bottom-0 left-5" : " ",
     ].join(" ")}>
       <div className="flex">
         <div className="flex-shrink-0">
-          {warning && <ExclamationIcon className="h-5 w-5 text-warningDark"/>}
-          {error && <ExclamationCircleIcon className="h-5 w-5 text-dangerDark"/>}
-          {success && <CheckCircleIcon className="h-5 w-5 text-successDark" aria-hidden="true"/>}
-          {!success && !error && !warning &&
+          {state === 'warning' && <ExclamationIcon className="h-5 w-5 text-warningDark"/>}
+          {state === 'error' && <ExclamationCircleIcon className="h-5 w-5 text-dangerDark"/>}
+          {state === 'success' && <CheckCircleIcon className="h-5 w-5 text-successDark" aria-hidden="true"/>}
+          {state === 'default' &&
           <InformationCircleIcon className="h-5 w-5 text-infoDark" aria-hidden="true"/>}
         </div>
         <div className="ml-3">
           <p className={["text-sm font-medium",
-            error ? "text-dangerDark" :
-              warning ? "text-warningDark" :
-                success ? "text-successDark" :
+            state === 'error' ? "text-dangerDark" :
+              state === 'warning' ? "text-warningDark" :
+                state === 'success' ? "text-successDark" :
                   "text-infoDark",
-          ].join(" ")}>{label}</p>
+          ].join(" ")}>
+            {label}
+          </p>
         </div>
         <div className="ml-auto pl-3">
           <div className="-mx-1.5 -my-1.5">
@@ -40,9 +42,9 @@ export const Alert = ({warning, error, success, label, topRight, topLeft, bottom
               type="button"
               className={["inline-flex rounded-md p-1.5 ",
                 "focus:outline-none",
-                error ? "text-dangerDark bg-dangerBackground hover:bg-dangerLight" :
-                  warning ? "text-warningDark bg-warningBackground hover:bg-warningLight" :
-                    success ? "text-successDark bg-successBackground hover:bg-successLight" :
+                state === 'error' ? "text-dangerDark bg-dangerBackground hover:bg-dangerLight" :
+                  state === 'warning' ? "text-warningDark bg-warningBackground hover:bg-warningLight" :
+                    state === 'success' ? "text-successDark bg-successBackground hover:bg-successLight" :
                       "text-infoDark bg-infoBackground hover:bg-infoLight",
               ].join(" ")}
             >
--- src/stories/alerts/Alert.stories.js
+++ src/stories/alerts/Alert.stories.js
@@ -4,15 +4,27 @@ import {Alert} from "./Alert";
 export default {
   title: "Elements/Alert",
   component: Alert,
-  argTypes: { backgroundColor: 'config'}
+  args: {
+    label: 'Wow! What a success!',
+    state: "success",
+    position: "topLeft",
+  },
+  argTypes: {
+    state: {control: {default: 'success', type: "select"}, options: ['success', 'error', 'warning', 'default']},
+    position: {control: {default: 'topLeft', type: "select"}, options: ['topRight', 'topLeft', 'bottomLeft']},
+  },
 }
+
+
+export const Playground = (args) => <Alert {...args}/>
+
 
 export const All = () => {
   return (
     <div className="m-4 space-y-3">
       <Alert success label="This has been a success!"/>
-      <Alert warning  label="This is a warning" />
-      <Alert error  label="This is an error"/>
+      <Alert warning label="This is a warning"/>
+      <Alert error label="This is an error"/>
       <Alert label="This is an alert"/>
     </div>
   )
@@ -21,5 +33,6 @@ export const Success = () => <Alert success label="This has been a success!"/>
 export const Warning = () => <Alert warning label="This is a warning"/>
 export const Error = () => <Alert error label="This is an error"/>
 export const Info = () => <Alert label="This is an info alert"/>
-export const MultiLine = () => <Alert label="This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, "/>
+export const MultiLine = () => <Alert
+  label="This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, This is an info alert, "/>
 
