c4c7e8a70b0928d99aca8db37f8134f4fb620935
jurrem <jurre@moosty.com>
2021-05-07 17:12:56 +0200

    footer details & DAO

---
+++ src/fixtures/footerItemsDAO.js
@@ -0,0 +1,24 @@
+
+export const FooterItemsDAO = {
+  liskcrowd: [
+    {label: "Read the blog", to: "https://lisk.io/blog/apps/building-lisk-dao-project"},
+    {label: "About the project team", to: "/"},
+    {label: "Github", to: "https://github.com/Moosty/"},
+  ],
+  lisk: [
+    {label: "What is Lisk?", to: "https://lisk.io/what-is-lisk"},
+    {label: "What is blockchain?", to: "https://lisk.io/what-is-blockchain"},
+    {label: "Lisk SDK documentation", to: "https://lisk.io/documentation/lisk-sdk/index.html"},
+    {label: "Join Lisk chat", to: "https://lisk.chat"},
+  ],
+  moosty: [
+    {label: "About the team", to: "https://moosty.com/"},
+    {label: "See projects", to: "https://moosty.com/lisk-ecosystem/"},
+    {label: "Get in touch", to: "https://moosty.com/contact/"},
+  ]
+}
+
+export const FooterAuthorDAO = {
+  name: "Xinrong Ding, Vitallity & Moosty",
+  url: "https://moosty.com"
+}
