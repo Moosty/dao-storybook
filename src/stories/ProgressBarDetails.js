50af548d67f806dca7ee7a664142cf7cac45b357
Raphael Cornelis <raphael@moosty.com>
2021-05-08 20:39:36 +0200

    'list'

--- src/stories/ProgressBarDetails.js
+++ src/stories/ProgressBarDetails.js
@@ -5,7 +5,7 @@ export const ProgressBarDetails = ({state,percentage,targetAmount, totalRaised, 
   return (
     <div className="w-full mb-1 flex flex-row justify-between items-center">
       <Typography type="body" Element="span">
-        <Typography type="h3" Element="span" className="text-themeNavBarBg">{current}</Typography>/{target}
+        <Typography type="bodyStrong" Element="span" className="text-themeNavBarBg">{current}</Typography>/{target}
         <Typography type="bodyStrong" Element="span"> {unit}</Typography></Typography>
       <Typography type="bodyStrong" Element="span">{((current / target) * 100).toFixed(2)}%</Typography>
     </div>
