3fadd5ca9c0fa23cf2251601690d48c1b66f62c6
jurrem <jurre@moosty.com>
2021-05-12 16:25:33 +0200

    stories

--- src/views/Crowd/MyProjects.js
+++ src/views/Crowd/MyProjects.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import {BreadCrumbs, Button, Container, FilterDao, FilterWrapper, Footer, NavBar} from "../../stories";
+import React, {useEffect, useState} from "react";
+import {BreadCrumbs, Button, Container, FilterDao, FilterWrapper, Footer, NavBar, Typography} from "../../stories";
 import {navBarArgs} from "../../fixtures/crowdfund/navbar";
 import {appWidth} from "../../shared/styles";
 import {projects} from "../../fixtures/crowdfund/projects";
@@ -7,10 +7,12 @@ import {CrowdCardContainer} from "../../stories/crowd/CrowdCardContainer";
 import {AccountProjectList} from "../../stories/AccountProjectList";
 import {FooterAuthor, FooterItems} from "../../fixtures/crowdfund/footerItems";
 import {AccountProjectSingleItem} from "../../stories/AccountProjectSingleItem";
+import {account} from "../../fixtures/crowdfund/account";
 
 export const MyProjects = () => {
   const [user, setUser] = useState("345733333743L");
   const [visible, setVisible] = useState(true);
+
 
   return (
     <>
@@ -24,6 +26,7 @@ export const MyProjects = () => {
       </Container>
       {visible &&
       <Container className={[appWidth, "space-x-4", "space-y-4", "flex", "flex-wrap", "flex-row", "my-10"].join(" ")}>
+        <Typography type="h1" Element="h1">My Crowdfunds</Typography>
         {projects && projects.filter(project => project.userAddress === user).map((project) =>
           <CrowdCardContainer {...project} />
         )}
@@ -36,10 +39,16 @@ export const MyProjects = () => {
         </AccountProjectList>
       </Container>}
       <Container className={[appWidth, "my-20"].join(" ")}>
+        <Typography type="h1" Element="h1">My Projects</Typography>
         <AccountProjectList>
-          {projects && projects.filter(project => project.userAddress != user).map((project) =>
+          {projects && projects.filter(project => project.userAddress === user).map((project) =>
             <AccountProjectSingleItem {...project}/>
           )}
+        </AccountProjectList>
+        <Typography type="h1" Element="h1">My Investments</Typography>
+        <AccountProjectList>
+          { account && account.crowd.funded.map(({crowdfund}) =>  <AccountProjectSingleItem {...projects.find(project => project.id === crowdfund)} account={account}/>)
+          }
         </AccountProjectList>
       </Container>
       <Footer items={FooterItems} author={FooterAuthor}></Footer>
