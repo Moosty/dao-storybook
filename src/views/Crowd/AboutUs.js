ce27cbb3ed8e3fd23ce25babb0f6462624d84f32
jurrem <jurre@moosty.com>
2021-05-14 21:27:20 +0200

    blogs

--- src/views/Crowd/AboutUs.js
+++ src/views/Crowd/AboutUs.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {BlogSection} from "../../stories/BlogSection";
-import {Button, Container, Footer, Form, NavBar, Typography} from "../../stories";
+import {Container, Footer, NavBar, Typography} from "../../stories";
 import {appWidth} from "../../shared/styles";
 import {FooterAuthor, FooterItems} from "../../fixtures/crowdfund/footerItems";
 import {navBarArgs} from "../../fixtures/crowdfund/navbar";
@@ -9,15 +9,20 @@ export const AboutUs = () => {
   return (
     <div>
       <NavBar {...navBarArgs} />
-      <Container className={[appWidth, "space-x-4", "flex", "flex-row"].join(" ")}>
-        <div className="lg:ml-4 my-4 ">
-          <Typography type="h2" Element='h2'>
-            About Us
+      <Container className={[appWidth, "flex flex-col lg:flex-row justify-between mt-4 lg:mt-10"].join(" ")}>
+        <div className="flex flex-col max-w-lg mb-4">
+        <Typography type="h1" Element="h1">About Lisk crowd</Typography>
+        <Typography type="body" Element="span">Lisk crowd is a proof of concept showing how blockchain protocols can be used to regulated crowdfunds in a decentralised way. It provides backers (investors) more control over their donation. In addition it forces the fundraiser to keep everyone updated. The fundraiser can allow periods of voting in which backers can cancel the project. When a project starts a crowdfund, they must indicate the amount of investment they need per period and how many periods are needed to realize their project. Hereafter we explain why it might be useful and how it works.
+        </Typography>
+        </div>
+        <div className="flex flex-col max-w-lg">
+          <Typography type="h1" Element="h1">About Moosty</Typography>
+          <Typography type="body" Element="span">Moosty is a multidisciplinary project team working on a diverse set of innovative solutions. Specifically focusing on blockchain/decentralization related initiatives. The team consists of three members: Sander (Corbifex), Raphael, Jurre. Most importantly we like working together and working with others. Create new things and new collaborations. Interested in working together? Reach out to us!
           </Typography>
         </div>
       </Container>
       <Container className={[appWidth, "space-x-4", "flex", "flex-row"].join(" ")}>
-        <BlogSection title="Read more about Moosty & Lisk" descriptionTop="Read more about the lisk application platform, or our other initiatives"/>
+        <BlogSection title="Blogs" descriptionTop=""/>
       </Container>
       <Footer items={FooterItems} author={FooterAuthor}></Footer>
 
