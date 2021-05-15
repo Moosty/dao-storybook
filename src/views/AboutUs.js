0b886f2b1cfec692c56a1547b361f57b17dab76b
jurrem <jurre@moosty.com>
2021-05-15 11:49:26 +0200

    hero

--- src/views/AboutUs.js
+++ src/views/AboutUs.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {BlogSection} from "../stories/BlogSection";
-import {Container, Footer, NavBar, Typography} from "../stories";
+import {Container, Footer, Hero, NavBar, Typography} from "../stories";
 import {appWidth} from "../shared/styles";
 import {FooterAuthorDAO, FooterItemsDAO} from "../fixtures/dao/footerItemsDAO";
 import {navBarArgsDAO} from "../fixtures/dao/navbar";
@@ -10,7 +10,12 @@ export const AboutUs = () => {
   return (
     <div>
       <NavBar {...navBarArgsDAO} />
-      <Container className={[appWidth, "flex flex-col lg:flex-row justify-between mt-4 max-w-7xl lg:mt-10"].join(" ")}>
+      <Hero
+        title="Kalipo | Decentralised Autonomous Organizations"
+        subTitle="Start to organise your world here"
+        buttonLabel2="Get in Touch"
+        buttonLabel1="Explore"
+      />      <Container className={[appWidth, "flex flex-col lg:flex-row justify-between mt-4 max-w-7xl lg:mt-10"].join(" ")}>
         <div className="flex flex-col max-w-lg mb-4">
           <Typography type="h1" Element="h1">Kalipo</Typography>
           <Typography type="body" Element="span">Voting is a valuable governance tool, it gives the members of a community the possibility to exert influence and also collects information about viewpoints. However, votings consume time and energy of the voters and a voting committee. This often results in poor voter attendance and even in invalid votes.
