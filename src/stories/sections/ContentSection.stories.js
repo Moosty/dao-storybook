e868c94c91f0d91d9453f82fb14f9720bf25c32a
jurrem <jurre@moosty.com>
2021-05-15 14:29:21 +0200

    sections

---
+++ src/stories/sections/ContentSection.stories.js
@@ -0,0 +1,76 @@
+import React from "react";
+import {ContentSection} from "./ContentSection";
+import {projectImages} from "../../shared/global.dao";
+
+export default {
+  title: "Sections/ContentSection",
+  component: ContentSection,
+}
+
+
+
+export const All = () => {
+  return (
+    <div>
+      <ContentSection gradient
+                      backgroundImage
+                      image={projectImages[6]}
+                      title="The New Way Of Crowdfunding"
+                      subTitle="more transparency, more structure, more success. "
+                      titleContent="What would you do?"
+                      content="Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst. Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.  "
+      />
+    </div>
+  )
+}
+
+
+export const Gradient = () => {
+  return (
+    <div>
+      <ContentSection gradient
+                      gradientClassName="bg-themeLight"
+                      title="The New Way Of Crowdfunding"
+                      subTitle="more transparency, more structure, more success. "
+                      titleContent="What would you do?"
+                      content="Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst. Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.  "
+                      image={projectImages[6]}
+                      backgroundImage
+      />
+    </div>
+  )
+}
+
+
+export const ContentButton = () => {
+  return (
+    <div>
+      <ContentSection gradient
+                      gradientClassName="bg-themeLight"
+                      title="The New Way Of Crowdfunding"
+                      subTitle="more transparency, more structure, more success. "
+                      titleContent="What would you do?"
+                      content="Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst. Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.  "
+                      image={projectImages[6]}
+                      backgroundImage
+                      buttonLabel1="Start Now!"
+      />
+    </div>
+  )
+}
+
+
+export const NoImage = () => {
+  return (
+    <div>
+      <ContentSection gradient
+                      title="The New Way Of Crowdfunding"
+                      subTitle="more transparency, more structure, more success. "
+                      titleContent="What would you do?"
+                      content="Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst. Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.Dit is een stukje tekst.Dit is een stukje tekst.  Dit is een stukje tekst.  Dit is een stukje tekst.  "
+                      buttonLabel1="Start Now!"
+                      onClickButton1={() => alert("Woowwja")}
+      />
+    </div>
+  )
+}
